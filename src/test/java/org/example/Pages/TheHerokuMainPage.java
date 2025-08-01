80ed4e2306f029625724132a4760b0e7c0f17806
Miczura <m.gieleta@gmail.com>
2025-08-01 09:26:20 +0200

    Updated main page

--- src/test/java/org/example/Pages/TheHerokuMainPage.java
+++ src/test/java/org/example/Pages/TheHerokuMainPage.java
@@ -1,6 +1,7 @@
 package org.example.Pages;
 
 import com.codeborne.selenide.SelenideElement;
+import org.openqa.selenium.By;
 
 
 import static com.codeborne.selenide.Selenide.$;
@@ -8,11 +9,14 @@ import static com.codeborne.selenide.Selenide.$;
 
 public class TheHerokuMainPage {
     private SelenideElement dropdownPageLink = $("a[href='/dropdown']");
-
+    private SelenideElement addRemoveElementLink = $(By.linkText("Add/Remove Elements"));
 
     public DropdownPage enterDropdownPage() {
         dropdownPageLink.click();
         return new DropdownPage();
     }
-
+    public AddRemoveElementsPage enterAddRemoveElementsPage(){
+        addRemoveElementLink.click();
+        return new AddRemoveElementsPage();
+    }
 }
